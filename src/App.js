fc8726e01bd265b930d831396fcc9e948d261303
ijs2000 <joshuvanani1999@gmail.com>
2023-06-22 09:40:31 -0400

    task2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import productdata from './data';
+import Home from './pages/Home';
+import 'bootstrap/dist/css/bootstrap.css';
+import { useState } from 'react';
+import Cart from './pages/Cart';
+import Navigation from "./components/Navigation";
+import Header from "./components/Header";
 
 function App() {
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItem = (item) => {
+        const getData = productdata.filter((inp)=> inp.id === item );
+        setCartItems([...cartItems, ...getData]);
+    }
+
+    const removeItem = (item) => {
+      console.log("remove item", item);
+      const fData = cartItems.filter((obj) => obj.id !== item);
+      console.log(fData);
+      setCartItems([...fData]);
+    }
+
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        
+    <Navigation cartItems={cartItems} />
+    <Header />
+    <Cart cartItems={cartItems} removeItem={removeItem} />
+    <Home productdata={productdata} addItem={addItem} cartItems={cartItems} />
+   
+   </>
+    
+  )
 }
 
 export default App;
